08bcaad12c847e94581f995a881c806735328e59
Daniel <danielcoc356@gmail.com>
2026-05-05 14:10:27 -0500

    starts benchmark feature development

---
+++ python/cnn_pipeline/src/benchmarking/benchmark.py
@@ -0,0 +1,6 @@
+# TODO: benchmarking system/module
+# that includes: time, memory usage, acuraccy , presicion, recall
+# experiments in mlflow + dagshub (dagshub.init(repo_owner=repo_owner, repo_name=repo_name, mlflow=True) )with
+# repo_owner: str = "MakabeD",
+# repo_name: str = "My-CNNs",
+# a function that runs a benchmarking loop, and an argument data path (a benchmark set)
