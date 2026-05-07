0d1236dbb85fcb5a316f3d97f54e3620e0ff8683
Daniel <danielcoc356@gmail.com>
2026-05-07 15:40:23 -0500

    feat: add load model function in benchmarking module, with pathlib's path-control

--- python/cnn_pipeline/src/benchmarking/benchmark.py
+++ python/cnn_pipeline/src/benchmarking/benchmark.py
@@ -1,6 +1,33 @@
-# TODO: benchmarking system/module
-# that includes: time, memory usage, acuraccy , presicion, recall
-# experiments in mlflow + dagshub (dagshub.init(repo_owner=repo_owner, repo_name=repo_name, mlflow=True) )with
-# repo_owner: str = "MakabeD",
-# repo_name: str = "My-CNNs",
-# a function that runs a benchmarking loop, and an argument data path (a benchmark set)
+import sys
+from pathlib import Path
+
+import torch
+
+ROOT_PATH = Path(__file__).parent.parent.parent
+sys.path.append(str(ROOT_PATH))
+from src.model.model import get_model
+from src.utils.config import Config, load_config
+
+
+def load_model(config: Config, model_path: Path):
+    model_name = config.model.name
+    num_classes = config.model.num_classes
+    pretrained = config.model.pretrained
+    dropout = config.model.dropout
+
+    print(model_path)
+    model = get_model(
+        model_name=model_name,
+        num_classes=num_classes,
+        pretrained=pretrained,
+        dropout=dropout,
+    )
+    state_dict = torch.load(model_path)
+    model.load_state_dict(state_dict)
+
+
+if __name__ == "__main__":
+    load_model(
+        config=load_config("./configs/xray-config1.yaml"),
+        model_path=Path("./models/chest-xray-own-best_model_0.2604.pt").resolve(),
+    )
