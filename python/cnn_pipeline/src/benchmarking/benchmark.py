48c90f43a25223a4a12d597435b35d3ec6ebfdfa
Daniel <danielcoc356@gmail.com>
2026-05-09 17:07:54 -0500

    feat: enhance benchmarking loop with timing metrics and CUDA synchronization

--- python/cnn_pipeline/src/benchmarking/benchmark.py
+++ python/cnn_pipeline/src/benchmarking/benchmark.py
@@ -1,17 +1,26 @@
 import sys
+import time
 from pathlib import Path
 
 import torch
 import torch.nn as nn
+from numpy import average
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
 ROOT_PATH = Path(__file__).parent.parent.parent
 sys.path.append(str(ROOT_PATH))
 from src.model.model import get_model
-from src.pipeline.data_pipeline import ImageDataset, get_transforms, load_statistics
+from src.pipeline.data_pipeline import (
+    ImageDataset,
+    get_device,
+    get_transforms,
+    load_statistics,
+)
 from src.train.training import compute_metrics
 from src.utils.config import Config, load_config
+
+DEVICE = get_device()
 
 
 def load_model(config: Config, model_path: Path) -> nn.Module:
@@ -70,19 +79,36 @@ def set_to_DataLoader(set: ImageDataset, batch_size: int, num_workers) -> DataLo
 def benchmarking_loop(dataloader: DataLoader, model: nn.Module):
     all_preds = []
     all_labels = []
+    all_times = []
     pbar = tqdm(dataloader)
-    model.eval()
+    model.eval().to(DEVICE)
     with torch.no_grad():
         for idx, (inputs, labels) in enumerate(pbar):
+            inputs, labels = inputs.to(DEVICE), labels.to(DEVICE)
+            _if_cuda()
+            start_time = time.perf_counter()
             output = model(inputs)
+            _if_cuda()
+            end_time = time.perf_counter()
+            all_times.append(end_time - start_time)
+            pbar.set_postfix({"time": f"{end_time - start_time:.4f}s"})
+
             _, resoult = torch.max(output.data, 1)
             all_preds.extend(resoult.cpu().tolist())
             all_labels.extend(labels.cpu().tolist())
-            print(resoult.data)
-            print(labels)
+        print(len(dataloader))
         y_true = torch.tensor(all_labels)
         y_pred = torch.tensor(all_preds)
+
+        average_time = sum(all_times) / len(dataloader)
+        print(average_time)
+        print(all_times)
         accuracy, precision, recall, f1_score = compute_metrics(y_true, y_pred)
+
+
+def _if_cuda():
+    if DEVICE == "cuda":
+        torch.cuda.synchronize()
 
 
 if __name__ == "__main__":
